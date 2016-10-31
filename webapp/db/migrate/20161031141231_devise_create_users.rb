2454b287b1dcce25de3e0f670ba19ffa23274db5
Matthew Bell <logicnotlogical@outlook.com>
2016-10-31 15:09:34 +0000

    Add name attribute to User model

--- webapp/db/migrate/20161031141231_devise_create_users.rb
+++ webapp/db/migrate/20161031141231_devise_create_users.rb
@@ -19,6 +19,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.inet     :current_sign_in_ip
       t.inet     :last_sign_in_ip
 
+      ## other stuff
+      t.string :name, null: false, default: ""
+
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
