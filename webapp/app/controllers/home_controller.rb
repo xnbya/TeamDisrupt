f2657f8a86a341d7140a4fdfc9436eb64bbbc988
Matthew Bell <logicnotlogical@outlook.com>
2016-10-31 14:36:51 +0000

    Add basic authentication

--- webapp/app/controllers/home_controller.rb
+++ webapp/app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+    before_action :authenticate_user!, only: [:home]
+
     def index
+      redirect_to :home if user_signed_in?
+    end
+
+    def home
     end
 end
