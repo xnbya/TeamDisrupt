7ae86d1eb647595c238871287afe38a33fb5f301
Fraser Savage <fraser_sav@yahoo.co.uk>
2016-11-01 19:07:08 +0000

    Some initial groundwork for ref-creation test

--- webapp/test/controllers/reference_controller_test.rb
+++ webapp/test/controllers/reference_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ReferenceControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.create(email: "hunter@test.io",
+                        encrypted_password: Devise::Encryptor.digest(User, 'hunter2'))
+  end
+
+  test "create reference" do
+    sign_in_as(@user)
+    @reference_controller = ReferenceController.new 
+    #@reference_controller.reference({title: "Test", note: "Hunter test", link: "http://cs.ucl.ac.uk", user: @user.id })
+    #@reference_controller.create
+    assert true
+  end
 end
