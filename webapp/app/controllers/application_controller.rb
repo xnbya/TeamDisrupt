c25f4aa4288380617b0c60ef14a18036fe4c9d78
Matthew Bell <logicnotlogical@outlook.com>
2016-11-01 21:50:12 +0000

    Add Bootstrap

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :set_nav
+
+  def set_nav
+    @nav_items = [
+      {
+        :name => "Home",
+        :url => root_path
+      }
+    ]
+
+    @nav_items_user = [
+      {
+        :name => "Home",
+        :url => home_path
+      },
+
+      {
+        :name => "References",
+        :url => references_path
+      }
+    ]
+  end
 end
