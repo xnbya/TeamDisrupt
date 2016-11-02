f2efb01f1112d4fcf71ca4c27d0ee4e10cc86171
Fraser Savage <fraser_sav@yahoo.co.uk>
2016-11-02 19:15:14 +0000

    initial test_signup using selenium

--- webapp/test/test_helper.rb
+++ webapp/test/test_helper.rb
@@ -1,18 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+#require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  
-  # Signs in with the user passed to it
-  def sign_in_as(user)
-    post user_session_path, params: { user: { email: user.email,
-                                              encrypted_password: Devise::Encryptor.digest(User, 'hunter2') } }
-    #follow_redirect!
-  end
-
 end
