8cafc482ef4a28267a7b6615e53644e66aefcc65
Alexis Enston <xn@outlook.com>
2016-11-03 12:51:13 +0000

    Add backup server to launch script with cron job

--- scripts/launchscripts/db-backup-server.sh
+++ scripts/launchscripts/db-backup-server.sh
@@ -3,12 +3,19 @@
 # script to setup database backup server
 # takes password of db server and ip address as arguments
 # defaults to backing up once a day
-# deletes backups older than 7 days
+# deletes backups older than 20 days
 # saves backups to /home/ubuntu/backups
 ######################
 
 dbpass=$1
 dbhost=$2
+dbname=$3
+
+mkdir ~/backups/
+echo "0 * * * * ~/TeamDisrupt/scripts/db-backup/db-backup.sh $dbhost 5432 $dbname pgadmin $dbpass ~/backups/ 20" | crontab -
+
+
+
 
 
 
